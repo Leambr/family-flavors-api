b9ca99da672bbf048bdf17d5f0a11a2456979295
Léa Mhoumadi <mhoumadi.lea@gmail.com>
2024-03-15 15:06:34 +0100

    refactor: auto increment id

--- dump/database.sql
+++ dump/database.sql
@@ -1,36 +1,36 @@
-CREATE TABLE IF NOT EXISTS dish_type (
-    `id` INT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS dishType (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
     `name` VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS season (
-    `id` INT PRIMARY KEY,
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
     `name` VARCHAR(50) NOT NULL,
-    `start_date` DATE NOT NULL,
-    `end_date` DATE NOT NULL
+    `startDate` DATE NOT NULL,
+    `endDate` DATE NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS `recipe` (
-    `id` INT PRIMARY KEY,
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
     `title` VARCHAR(255) NOT NULL,
-    `diet_type` VARCHAR(50) NOT NULL,
+    `dietType` VARCHAR(50) NOT NULL,
     `serving` SMALLINT NOT NULL,
-    `prep_time` INT NOT NULL,
-    `cook_time` INT,
+    `prepTime` INT NOT NULL,
+    `cookTime` INT,
     `instruction` TEXT NOT NULL,
-    `image_url` VARCHAR(255),
+    `imageUrl` VARCHAR(255),
     `season_id` INT,
-    `dish_type_id` INT,
+    `dishType_id` INT,
     CONSTRAINT fk_recipe_season FOREIGN KEY (season_id) REFERENCES season(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT fk_recipe_dish_type FOREIGN KEY (dish_type_id) REFERENCES dish_type(id) ON DELETE CASCADE ON UPDATE CASCADE
+    CONSTRAINT fk_recipe_dishType FOREIGN KEY (dishType_id) REFERENCES dishType(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 CREATE TABLE ingredient (
-    `id` INT PRIMARY KEY,
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
     `name` VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE recipe_ingredient_measure (
-    `id` INT PRIMARY KEY,
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
     `unit` VARCHAR(50) NOT NULL, 
     `amount` DECIMAL(10, 2) NOT NULL  
 );
@@ -45,3 +45,5 @@ CREATE TABLE recipe_ingredient (
     FOREIGN KEY (ingredient_id) REFERENCES ingredient(id),
     FOREIGN KEY (recipe_ingredient_measure_id) REFERENCES recipe_ingredient_measure(id)
 );
+
+INSERT INTO `dishType` (`id`, `name`) VALUES (1, 'Main Course');
